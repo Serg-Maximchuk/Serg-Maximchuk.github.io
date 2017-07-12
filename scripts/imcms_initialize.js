aea46e3be5e958e193ace826db85e6b7730172a5
FrontendDeveloper <chygura.v@gmail.com>
2017-07-12 18:26:52 +0300

    added last changes from old repository

--- scripts/imcms_initialize.js
+++ scripts/imcms_initialize.js
@@ -1,14 +1,14 @@
-(function ( Imcms ) {
-  Imcms.Select.init();
-  Imcms.NumberBox.init();
-  Imcms.Button.init();
-  Imcms.DatePicker.init();
-  Imcms.TimePicker.init();
-  Imcms.Keyword.init();
-  Imcms.AdminPanel.init();
-  Imcms.Folders.init();
-  Imcms.ChooseImg.init();
-  Imcms.ContentManager.init();
-  Imcms.ImageEditor.init();
-  Imcms.MenuEditor.init();
-})( Imcms );
+(function (Imcms) {
+    Imcms.Select.init();
+    Imcms.NumberBox.init();
+    Imcms.Button.init();
+    Imcms.DatePicker.init();
+    Imcms.TimePicker.init();
+    Imcms.Keyword.init();
+    Imcms.AdminPanel.init();
+    Imcms.Folders.init();
+    Imcms.ChooseImg.init();
+    Imcms.ContentManager.init();
+    Imcms.ImageEditor.init();
+    Imcms.MenuEditor.init();
+})(Imcms);
