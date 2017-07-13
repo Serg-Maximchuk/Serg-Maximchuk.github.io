1c6a80b56165012f13fb0312ae12e8202ca78ccc
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-13 12:51:17 +0300

    Implemented crossbrowser XMLHttpRequest creating.

--- scripts/imcms_main.js
+++ scripts/imcms_main.js
@@ -14,3 +14,27 @@ Imcms.dependencies = {
         path: "./libs/jquery.mask.min.js"
     }
 };
+Imcms.createXMLHttpRequest = function () {
+    if (typeof XMLHttpRequest !== 'undefined') {
+        return new XMLHttpRequest();
+    }
+    // old browsers support
+    var versions = [
+        "MSXML2.XmlHttp.6.0",
+        "MSXML2.XmlHttp.5.0",
+        "MSXML2.XmlHttp.4.0",
+        "MSXML2.XmlHttp.3.0",
+        "MSXML2.XmlHttp.2.0",
+        "Microsoft.XmlHttp"
+    ];
+
+    var xhr;
+    for (var i = 0; i < versions.length; i++) {
+        try {
+            xhr = new ActiveXObject(versions[i]);
+            break;
+        } catch (e) {
+        }
+    }
+    return xhr;
+};
