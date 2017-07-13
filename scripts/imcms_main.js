47377275a96ae344f8c1dc1ab7002a41952917cc
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-13 12:50:06 +0300

    Started implementing modules dependencies by adding jquery and jquery.mask

--- scripts/imcms_main.js
+++ scripts/imcms_main.js
@@ -2,3 +2,15 @@ Imcms = {};
 Imcms.modules = {
     imcms: Imcms // default module
 };
+Imcms.dependencies = {
+    "jquery": {
+        path: "//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js",
+        factory: function () {
+            return $.noConflict(true);
+        }
+    },
+    "jquery-mask": {
+        dependencies: ["jquery"],
+        path: "./libs/jquery.mask.min.js"
+    }
+};
