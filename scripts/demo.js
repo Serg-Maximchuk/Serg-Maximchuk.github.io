6d4da1d36117dad7e6ab1d8ab97c83ce14e08d71
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-21 19:13:00 +0300

    - Using basic modules on index.html page instead of all modules.

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,9 +2,22 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 21.07.17.
  */
-Imcms.require(["imcms-flags", "imcms-tests", "jquery"], function (imcmsFlags, tests, $) {
-    console.info("%c Tests loaded.", "color: green");
-    Imcms.tests = tests;
-    imcmsFlags.init();
-    console.timeEnd("imCMS JS loaded");
-});
+Imcms.require(
+    [
+        "imcms-flags", "imcms-date-picker", "imcms-time-picker", "imcms-select", "imcms-numberbox", "imcms-keyword",
+        "imcms-tests", "jquery"
+    ],
+    function (imcmsFlags, imcmsDatePicker, imcmsTimePicker, imcmsSelect, imcmsNumberbox, imcmsKeyword, tests, $) {
+        console.info("%c Tests loaded.", "color: green");
+        Imcms.tests = tests;
+
+        imcmsFlags.init();
+        imcmsDatePicker.init();
+        imcmsTimePicker.init();
+        imcmsSelect.init();
+        imcmsNumberbox.init();
+        imcmsKeyword.init();
+
+        console.timeEnd("imCMS JS loaded");
+    }
+);
