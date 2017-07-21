eb6e72584aa66de5bbf53af699aef47beda6ccb2
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-21 15:19:52 +0300

    - Refactored admin panel module.

--- scripts/admin/imcms_admin_panel.js
+++ scripts/admin/imcms_admin_panel.js
@@ -1,5 +1,9 @@
 Imcms.define("imcms-admin-panel", ["imcms-pop-up", "jquery"], function (imcmsPopUp, $) {
 
+    function logNotImplementedFeature(feature) {
+        console.log("%c Not implemented feature: " + feature, "color: red;");
+    }
+
     function setShowPanelRule() {
         $(document).mousemove(function (event) {
             if (event.pageY >= 0 && event.pageY <= 15) { // fixme: what is 15 ???
@@ -31,19 +35,99 @@ Imcms.define("imcms-admin-panel", ["imcms-pop-up", "jquery"], function (imcmsPop
         $(".imcms-admin").css({"top": "" + px + "px"});
     }
 
-    function showPageInfo(e) {
-        e.preventDefault();
-
+    function showPageInfo() {
         var $popup = $(".imcms-pop-up-modal[data-menu=pageInfo]").css({"display": "block"});
-
         imcmsPopUp.init($popup);
+    }
+
+    var viewModel = {
+        adminPanelElements: [
+            {
+                name: "flags",
+                selector: ".imcms-flags__flag.imcms-flag",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("flags click");
+                }
+            },
+            {
+                name: "public view",
+                selector: ".imcms-menu__item.imcms-menu__item--public",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("public view click");
+                }
+            },
+            {
+                name: "edit",
+                selector: ".imcms-menu__item.imcms-menu__item--edit",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("edit click");
+                }
+            },
+            {
+                name: "preview",
+                selector: ".imcms-menu__item.imcms-menu__item--preview",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("preview click");
+                }
+            },
+            {
+                name: "publish offline version",
+                selector: ".imcms-menu__item.imcms-menu__item--publish-of",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("publish offline version click");
+                }
+            },
+            {
+                name: "page info",
+                selector: ".imcms-menu__item.imcms-menu__item--page-info",
+                onClick: function (e) {
+                    e.preventDefault();
+                    showPageInfo();
+                }
+            },
+            {
+                name: "document editor",
+                selector: ".imcms-menu__item.imcms-menu__item--document",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("document editor click");
+                }
+            },
+            {
+                name: "admin",
+                selector: ".imcms-menu__item.imcms-menu__item--admin",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("admin click");
+                }
+            },
+            {
+                name: "admin",
+                selector: ".imcms-menu__item.imcms-menu__item--logout .imcms-button",
+                onClick: function (e) {
+                    e.preventDefault();
+                    logNotImplementedFeature("logout click");
+                }
+            }
+        ]
+    };
+
+    function setPanelClicks($panel) {
+        viewModel.adminPanelElements.forEach(function (panelData) {
+            $panel.find(panelData.selector).click(panelData.onClick);
+        });
     }
 
     return {
         init: function () {
             setShowPanelRule();
             setHidePanelRule();
-            $(".imcms-admin").find(".imcms-menu__item--page-info").click(showPageInfo);
+            setPanelClicks($(".imcms-admin-panel"));
         }
     }
 });
