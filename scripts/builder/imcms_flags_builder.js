83df1fa1c21a3fae9bd08a73cacb15fd102c1f6f
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-24 15:33:13 +0300

    - Implemented flags builder logic.

--- scripts/builder/imcms_flags_builder.js
+++ scripts/builder/imcms_flags_builder.js
@@ -3,5 +3,61 @@
  * 24.07.17.
  */
 Imcms.define("imcms-flags-builder", ["jquery"], function ($) {
+    var FLAGS_CLASS = "imcms-flag",
+        CONTAINER_CLASS = "imcms-flags",
+        FLAG_ACTIVE_CLASS = FLAGS_CLASS + "--" + "active"
+    ;
 
+    function changeNeighborFlag($btn) {
+        var $neighborFlag = ($btn.next("." + FLAGS_CLASS).length !== 0)
+            ? $btn.next("." + FLAGS_CLASS)
+            : $btn.prev("." + FLAGS_CLASS);
+
+        $neighborFlag.toggleClass(FLAG_ACTIVE_CLASS);
+    }
+
+    function onFlagClick(event) {
+        var $clickedFlag = $(this);
+
+        if (!$clickedFlag.hasClass(FLAGS_CLASS)) {
+            return;
+        }
+
+        if ($clickedFlag.hasClass(FLAG_ACTIVE_CLASS)) {
+            event.preventDefault();
+
+        } else {
+            $clickedFlag.addClass(FLAG_ACTIVE_CLASS);
+            changeNeighborFlag($clickedFlag);
+        }
+    }
+
+    function flagsBuilder(lang) {
+        return function (tag, attributesObj) {
+            attributesObj = attributesObj || {};
+            attributesObj["class"] = FLAGS_CLASS + " " + FLAGS_CLASS + "--" + lang
+                + (attributesObj.active ? " " + FLAG_ACTIVE_CLASS : "")
+                + (attributesObj["class"] ? " " + attributesObj["class"] : "");
+
+            delete attributesObj.active;
+            return $(tag, attributesObj).click(onFlagClick);
+        };
+    }
+
+    return {
+        "class": FLAGS_CLASS,
+        "container-class": CONTAINER_CLASS,
+        eng: flagsBuilder("en"),
+        swe: flagsBuilder("sw"),
+        container: function (tag, attributesObj, elements) {
+            attributesObj = attributesObj || {};
+            attributesObj["class"] = CONTAINER_CLASS + (attributesObj["class"] ? " " + attributesObj["class"] : "");
+
+            elements = elements.map(function (element) {
+                return element.addClass(CONTAINER_CLASS + "__flag");
+            });
+
+            return $(tag, attributesObj).append(elements);
+        }
+    }
 });
--- scripts/demo.js
+++ scripts/demo.js
@@ -4,14 +4,87 @@
  */
 Imcms.require(
     [
-        "imcms-flags", "imcms-date-picker", "imcms-time-picker", "imcms-select", "imcms-numberbox", "imcms-keyword",
-        "imcms-tests", "jquery"
+        "imcms-date-picker", "imcms-time-picker", "imcms-select", "imcms-numberbox", "imcms-keyword", "imcms-tests",
+        "imcms-components-builder", "jquery"
     ],
-    function (imcmsFlags, imcmsDatePicker, imcmsTimePicker, imcmsSelect, imcmsNumberbox, imcmsKeyword, tests, $) {
+    function (imcmsDatePicker, imcmsTimePicker, imcmsSelect, imcmsNumberbox, imcmsKeyword, tests, componentsBuilder, $) {
         console.info("%c Tests loaded.", "color: green");
         Imcms.tests = tests;
 
-        imcmsFlags.init();
+        // buttons
+
+        var buttons = componentsBuilder.buttons,
+            $negativeBtn = buttons.negative("<button>", {
+                type: "button",
+                text: "negative"
+            }),
+            $positiveBtn = buttons.positive("<button>", {
+                type: "button",
+                text: "positive"
+            }),
+            $neutralBtn = buttons.neutral("<button>", {
+                type: "button",
+                text: "neutral"
+            }),
+            $saveBtn = buttons.save("<button>", {
+                type: "button",
+                text: "save"
+            }),
+            $negativeBtn2 = buttons.negative("<button>", {
+                type: "button",
+                text: "negative"
+            }),
+            $positiveBtn2 = buttons.positive("<button>", {
+                type: "button",
+                text: "positive"
+            }),
+            $neutralBtn2 = buttons.neutral("<button>", {
+                type: "button",
+                text: "neutral"
+            }),
+            $saveBtn2 = buttons.save("<button>", {
+                type: "button",
+                text: "save"
+            }),
+            $buttonsRow2 = buttons.container("<div>", {
+                id: "buttons-container-example"
+            }, [
+                $negativeBtn2,
+                $positiveBtn2,
+                $neutralBtn2,
+                $saveBtn2
+            ])
+        ;
+
+        $("#buttons-example").append($negativeBtn)
+            .append($positiveBtn)
+            .append($neutralBtn)
+            .append($saveBtn)
+            .append($buttonsRow2);
+
+        // flags
+
+        var flags = componentsBuilder.flags,
+            $engFlag = flags.eng("<div>", {
+                active: true
+            }),
+            $engFlag2 = flags.eng("<div>"),
+            $sweFlag = flags.swe("<div>"),
+            $sweFlag2 = flags.swe("<div>", {
+                active: true
+            }),
+            $flagsRow = flags.container("<div>", {
+                id: "flags-container-example"
+            }, [
+                $engFlag2,
+                $sweFlag2
+            ])
+        ;
+
+        $("#flags-example").append($engFlag)
+            .append($sweFlag)
+            .append($flagsRow);
+
         imcmsDatePicker.init();
         imcmsTimePicker.init();
         imcmsSelect.init();
