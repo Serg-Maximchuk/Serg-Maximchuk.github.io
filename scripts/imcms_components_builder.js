e3bfb17a05d775c5c43bae51c80c49139ff0ffdd
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-24 15:10:36 +0300

    Created imcms components builder module.

---
+++ scripts/imcms_components_builder.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 24.07.17.
+ */
+Imcms.define("imcms-components-builder", ["imcms-buttons-builder", "imcms-flags-builder"], function (buttons, flags) {
+    return {
+        buttons: buttons,
+        flags: flags
+    };
+});
