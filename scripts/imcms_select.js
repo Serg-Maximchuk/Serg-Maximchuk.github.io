9e71026d3c6e09fc0eca8e089d54fd115485d697
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-25 15:01:30 +0300

    Upgraded select closing logic.

--- scripts/imcms_select.js
+++ scripts/imcms_select.js
@@ -2,8 +2,7 @@ Imcms.define("imcms-select", ["jquery"], function ($) {
     function toggleSelect() {
         var $this = $(this),
             select = $this.closest(".imcms-select").find(".imcms-drop-down-list"),
-            dropDownItem = select.children(".imcms-drop-down-list__items")
-                                 .find(".imcms-drop-down-list__item")
+            dropDownItem = select.children(".imcms-drop-down-list__items").find(".imcms-drop-down-list__item")
         ;
 
         select.toggleClass("imcms-select__drop-down-list--active");
@@ -34,6 +33,7 @@ Imcms.define("imcms-select", ["jquery"], function ($) {
     return {
         init: function () {
             $(".imcms-drop-down-list__select-item").click(toggleSelect);
+            $(document).click(closeSelect);
         }
     };
 });
