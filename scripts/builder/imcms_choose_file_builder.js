1b369f9b6b1d650bc42fb8e0e1711ae06969c939
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-26 18:36:58 +0300

    Created empty choose file builder module.

---
+++ scripts/builder/imcms_choose_file_builder.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 26.07.17.
+ */
+Imcms.define("imcms-choose-file-builder", ["imcms-bem-builder"], function (BEM) {
+    return {}
+});
