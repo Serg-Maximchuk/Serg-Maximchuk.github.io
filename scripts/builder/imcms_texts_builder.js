53374bdff94d65205c03b10b4bb09e164742998f
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 17:17:25 +0300

    Implemented creating text with colors in texts builder module.

--- scripts/builder/imcms_texts_builder.js
+++ scripts/builder/imcms_texts_builder.js
@@ -198,6 +198,15 @@ Imcms.define("imcms-texts-builder", ["imcms-bem-builder", "jquery"], function (B
                 {"label": $label},
                 {"input-box": $inputBox}
             ]);
+        },
+        error: function (tag, text, attributes) {
+            return $(tag, attributes).addClass("imcms-error-msg").text(text);
+        },
+        info: function (tag, text, attributes) {
+            return $(tag, attributes).addClass("imcms-info-msg").text(text);
+        },
+        title: function (tag, text, attributes) {
+            return $(tag, attributes).addClass("imcms-title").text(text);
         }
     }
 });
