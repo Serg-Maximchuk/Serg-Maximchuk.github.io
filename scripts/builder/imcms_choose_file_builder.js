fe6f1c744accf6738bde1ff67931fb33a381bbd7
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 10:56:27 +0300

    Implemented choose file element builder.

--- scripts/builder/imcms_choose_file_builder.js
+++ scripts/builder/imcms_choose_file_builder.js
@@ -3,5 +3,39 @@
  * 26.07.17.
  */
 Imcms.define("imcms-choose-file-builder", ["imcms-bem-builder"], function (BEM) {
-    return {}
+    var buttonBEM = new BEM({
+        block: "imcms-button",
+        elements: {
+            "item": ""
+        }
+    });
+
+    var chooseFileBEM = new BEM({
+        block: "imcms-choose-file",
+        elements: {
+            "button": "imcms-button",
+            "file-name": ""
+        }
+    });
+
+    return {
+        container: function (tag, attributes) {
+            var $input = buttonBEM.buildElement("item", "<div>", {
+                    id: attributes.id,
+                    name: attributes.name,
+                    type: "file"
+                }, ["invisible"]),
+
+                $label = buttonBEM.buildBlock("<label>", [{"item": $input}], {
+                    "for": attributes.id
+                }),
+
+                $fileName = chooseFileBEM.buildElement("file-name", "<span>");
+
+            return chooseFileBEM.buildBlock("<div>", [
+                {"button": $label},
+                {"file-name": $fileName}
+            ]);
+        }
+    }
 });
