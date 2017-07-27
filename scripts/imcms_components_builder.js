0b6f36633f125ecf0823adb21d24bf0ef6a270e4
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 12:12:54 +0300

    Added choose image module to components builder.

--- scripts/imcms_components_builder.js
+++ scripts/imcms_components_builder.js
@@ -5,16 +5,17 @@
 Imcms.define("imcms-components-builder",
     [
         "imcms-buttons-builder", "imcms-flags-builder", "imcms-checkboxes-builder", "imcms-radio-buttons-builder",
-        "imcms-selects-builder", "imcms-texts-builder"
+        "imcms-selects-builder", "imcms-texts-builder", "imcms-choose-image-builder"
     ],
-    function (buttons, flags, checkboxes, radios, selects, texts) {
+    function (buttons, flags, checkboxes, radios, selects, texts, chooseImage) {
         return {
             buttons: buttons,
             flags: flags,
             checkboxes: checkboxes,
             radios: radios,
             selects: selects,
-            texts: texts
+            texts: texts,
+            chooseImage: chooseImage
         };
     }
 );
