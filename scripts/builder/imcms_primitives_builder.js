b03a1873f2a548b1d34bcf671c9f881be898d257
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 17:45:08 +0300

    Created new primitives builder module.

---
+++ scripts/builder/imcms_primitives_builder.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 27.07.17.
+ */
+Imcms.define("imcms-primitives-builder", ["imcms-labels-builder"], function (labels) {
+    return {
+        labels: labels
+    }
+});
