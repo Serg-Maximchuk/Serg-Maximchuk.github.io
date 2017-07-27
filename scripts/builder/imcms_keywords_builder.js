ef495e4952d0e39e32617a5558e209937b46e6ab
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 14:44:47 +0300

    Created empty keywords module.

---
+++ scripts/builder/imcms_keywords_builder.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 27.07.17.
+ */
+Imcms.define("imcms-keywords-builder",
+    ["imcms-bem-builder", "imcms-texts-builder", "imcms-buttons-builder", "jquery"],
+    function (BEM, texts, buttons, $) {
+        return {}
+    }
+);
