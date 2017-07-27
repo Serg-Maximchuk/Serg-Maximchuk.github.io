f5ef3b0a03336bee0b5fe762320e4ce047c01335
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-27 17:50:58 +0300

    Used new labels building logic.

--- scripts/builder/imcms_radio_buttons_builder.js
+++ scripts/builder/imcms_radio_buttons_builder.js
@@ -2,42 +2,44 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 25.07.17.
  */
-Imcms.define("imcms-radio-buttons-builder", ["imcms-bem-builder"], function (bemBuilder) {
-    var radioBEM = new bemBuilder({
-            block: "imcms-radio",
-            elements: {
-                "label": "imcms-label",
-                "input": "imcms-input"
-            }
-        }),
-        containerBEM = new bemBuilder({
-            block: "imcms-radios",
-            elements: {
-                "radio": "imcms-radio"
-            }
-        })
-    ;
+Imcms.define("imcms-radio-buttons-builder",
+    ["imcms-bem-builder", "imcms-primitives-builder"],
+    function (bemBuilder, primitives) {
+        var radioBEM = new bemBuilder({
+                block: "imcms-radio",
+                elements: {
+                    "input": "imcms-input"
+                }
+            }),
+            containerBEM = new bemBuilder({
+                block: "imcms-radios",
+                elements: {
+                    "radio": "imcms-radio"
+                }
+            })
+        ;
 
-    return {
-        radio: function (tag, attributes) {
-            var $input = radioBEM.buildElement("input", "<input>", {
-                type: "radio",
-                name: attributes.name,
-                id: attributes.id,
-                checked: attributes.checked
-            });
-            var $label = radioBEM.buildElement("label", "<label>", {
-                "for": attributes.id,
-                text: attributes.text,
-                click: attributes.click
-            });
-            return radioBEM.buildBlock(tag, [
-                {"input": $input},
-                {"label": $label}
-            ]);
-        },
-        container: function (tag, attributesObj, elements) {
-            return containerBEM.buildBlock(tag, elements, attributesObj, "radio");
+        return {
+            radio: function (tag, attributes) {
+                var $input = radioBEM.buildElement("input", "<input>", {
+                    type: "radio",
+                    name: attributes.name,
+                    id: attributes.id,
+                    checked: attributes.checked
+                });
+                var $label = primitives.labels.imcmsLabel({
+                    "for": attributes.id,
+                    text: attributes.text,
+                    click: attributes.click
+                });
+                return radioBEM.buildBlock(tag, [
+                    {"input": $input},
+                    {"label": $label}
+                ]);
+            },
+            container: function (tag, attributesObj, elements) {
+                return containerBEM.buildBlock(tag, elements, attributesObj, "radio");
+            }
         }
     }
-});
+);
