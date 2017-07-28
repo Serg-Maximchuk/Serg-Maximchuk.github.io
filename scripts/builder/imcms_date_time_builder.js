981ea7cc5c7504961ea87bfcaad97691531f064a
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-28 11:32:01 +0300

    Implemented not editable date box.

--- scripts/builder/imcms_date_time_builder.js
+++ scripts/builder/imcms_date_time_builder.js
@@ -2,6 +2,46 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 28.07.17.
  */
-Imcms.define("imcms-date-time-builder", ["imcms-bem-builder", "imcms-texts-builder"], function (BEM, texts) {
-    return {};
+Imcms.define("imcms-date-time-builder", ["imcms-bem-builder"], function (BEM) {
+    var fieldWrapperBEM = new BEM({
+            block: "imcms-field",
+            elements: {
+                "date-picker": "imcms-date-picker"
+            }
+        }),
+        dateMainContainerBEM = new BEM({
+            block: "imcms-date-picker",
+            elements: {
+                "current-date": "imcms-current-date"
+            }
+        }),
+        dateInputContainerBEM = new BEM({
+            block: "imcms-current-date",
+            elements: {
+                "input": ""
+            }
+        })
+    ;
+    return {
+        dateBoxReadOnly: function (tag, attributes) {
+            var $dateInput = dateInputContainerBEM.buildElement("input", "<input>", {
+                    type: "text",
+                    title: attributes.title,
+                    placeholder: attributes.placeholder,
+                    autocomplete: "off",
+                    readonly: "readonly"
+                }),
+                $dateInputContainer = dateInputContainerBEM.buildBlock("<div>", [
+                    {"input": $dateInput}
+                ]),
+                $dateMainContainer = dateMainContainerBEM.buildBlock("<div>", [
+                    {"current-date": $dateInputContainer}
+                ])
+            ;
+
+            return fieldWrapperBEM.buildBlock("<div>", [
+                {"date-picker": $dateMainContainer}
+            ]);
+        }
+    };
 });
