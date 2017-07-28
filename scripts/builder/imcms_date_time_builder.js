6fd075833573dfc85f32ef06f9d7224e48995200
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-07-28 10:46:28 +0300

    - Created date-time builder module file. - Updated demo page structure.

---
+++ scripts/builder/imcms_date_time_builder.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 28.07.17.
+ */
+Imcms.define("imcms-date-time-builder", ["imcms-bem-builder", "imcms-texts-builder"], function (BEM, texts) {
+    return {};
+});
