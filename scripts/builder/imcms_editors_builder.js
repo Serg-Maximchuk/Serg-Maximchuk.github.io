cf16a8b3b85167df03761e8313a4b47b267d4cd6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-07 18:05:54 +0300

    Created unified editors builder.

---
+++ scripts/builder/imcms_editors_builder.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 07.08.17.
+ */
+Imcms.define("imcms-editors-builder", ["imcms-page-info-builder"], function (pageInfoBuilder) {
+    function buildByStrategy(strategy) {
+        return strategy.build();
+    }
+
+    return {
+        buildPageInfo: function () {
+            return buildByStrategy(pageInfoBuilder);
+        }
+    }
+});
