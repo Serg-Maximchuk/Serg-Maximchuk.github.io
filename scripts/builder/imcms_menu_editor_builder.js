d4cea28e27b693c731da584ebd702558855b8029
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-11 16:58:59 +0300

    Implemented simple menu editor content builder.

--- scripts/builder/imcms_menu_editor_builder.js
+++ scripts/builder/imcms_menu_editor_builder.js
@@ -42,6 +42,97 @@ Imcms.define("imcms-menu-editor-builder",
         }
 
         function buildBody() {
+
+            function buildMenuItemControls() {
+                var menuControlsBEM = new BEM({
+                    block: "imcms-controls",
+                    elements: {"control": "imcms-control"}
+                });
+
+                var $controlMove = menuControlsBEM.buildElement("control", "<div>", {}, ["move"]);
+                var $controlRemove = menuControlsBEM.buildElement("control", "<div>", {}, ["remove"]);
+                var $controlRename = menuControlsBEM.buildElement("control", "<div>", {}, ["rename"]);
+
+                return menuControlsBEM.buildBlock("<div>", [
+                    {"control": $controlMove},
+                    {"control": $controlRemove},
+                    {"control": $controlRename}
+                ]);
+            }
+
+            function buildMenuItems() {
+                var menuItemBEM = new BEM({
+                    block: "imcms-menu-item",
+                    elements: {
+                        "info": "imcms-title imcms-grid-coll-2",
+                        "controls": "imcms-controls"
+                    }
+                });
+
+                var $menuItemId = menuItemBEM.buildElement("info", "<div>", {text: "1001"});
+                var $menuItemTitle = menuItemBEM.buildElement("info", "<div>", {text: "Start page"});
+                var $controls = buildMenuItemControls();
+
+                return menuItemBEM.buildBlock("<div>", [
+                    {"info": $menuItemId},
+                    {"info": $menuItemTitle},
+                    {"controls": $controls}
+                ]);
+            }
+
+            function buildMenuElements() {
+                var menuTreeBEM = new BEM({
+                    block: "imcms-menu-items-tree",
+                    elements: {"menu-items": "imcms-menu-items"}
+                });
+
+                var menuItemsBEM = new BEM({
+                    block: "imcms-menu-items",
+                    elements: {"menu-item": "imcms-menu-item"}
+                });
+
+                var $testMenuItem = buildMenuItems();
+
+                var $menuItems = menuItemsBEM.buildBlock("<div>", [{"menu-item": $testMenuItem}], {
+                    "data-menu-items-lvl": "1"
+                });
+
+                return menuTreeBEM.buildBlock("<div>", [{"menu-items": $menuItems}]);
+            }
+
+            function buildMenuTitlesRow() {
+                var titlesBEM = new BEM({
+                    block: "imcms-menu-list-titles",
+                    elements: {"title": "imcms-grid-coll-2"}
+                });
+
+                var $idColumnHead = titlesBEM.buildElement("title", "<div>", {text: "id"});
+                var $titleColumnHead = titlesBEM.buildElement("title", "<div>", {text: "Title"});
+
+                return titlesBEM.buildBlock("<div>", [
+                    {"title": $idColumnHead},
+                    {"title": $titleColumnHead}
+                ]);
+            }
+
+            function buildMenuList() {
+                var menuListBEM = new BEM({
+                    block: "imcms-menu-list",
+                    elements: {
+                        "titles": "imcms-menu-list-titles",
+                        "items": "imcms-menu-items-tree"
+                    }
+                });
+
+                var $titles = buildMenuTitlesRow();
+                var $menuList = buildMenuElements();
+
+                return menuListBEM.buildBlock("<div>", [
+                    {"titles": $titles},
+                    {"items": $menuList}
+                ]);
+            }
+
             var bodyBEM = new BEM({
                 block: "imcms-menu-editor-body",
                 elements: {
@@ -50,7 +141,8 @@ Imcms.define("imcms-menu-editor-builder",
                 }
             });
 
-            var $menuElementsContainer = bodyBEM.buildElement("left-side", "<div>"); // empty for now
+            var $menuList = buildMenuList();
+            var $menuElementsContainer = bodyBEM.buildElement("left-side", "<div>").append($menuList);
             var $documentsContainer = bodyBEM.buildElement("right-side", "<div>"); // todo: should be document editor
 
             return bodyBEM.buildBlock("<div>", [
