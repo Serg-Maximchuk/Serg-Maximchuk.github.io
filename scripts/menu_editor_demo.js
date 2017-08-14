3436ce1735e83f6507e932638b211175067c1f82
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-14 17:17:07 +0300

    No more old js used on menu editor page.

--- scripts/menu_editor_demo.js
+++ scripts/menu_editor_demo.js
@@ -2,11 +2,10 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 10.08.17.
  */
-Imcms.require(["imcms-editors-builder", "imcms-initialize", "imcms-tests"], function (builder, imcms, tests) {
+Imcms.require(["imcms-editors-builder", "imcms-tests"], function (builder, tests) {
     builder.buildMenuEditor();
 
     console.info("%c Tests loaded.", "color: green");
     Imcms.tests = tests;
-    imcms.init();
     console.timeEnd("imCMS JS loaded");
 });
