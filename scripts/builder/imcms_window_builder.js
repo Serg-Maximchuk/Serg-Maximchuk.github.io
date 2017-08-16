0e87a7e5135e2ce8d34d9cd6bf89e227024501cf
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-16 12:08:57 +0300

    Implemented window head build function.

--- scripts/builder/imcms_window_builder.js
+++ scripts/builder/imcms_window_builder.js
@@ -2,6 +2,24 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 16.08.17.
  */
-Imcms.define("imcms-window-builder", [], function () {
-    return {};
+Imcms.define("imcms-window-builder", ["imcms-bem-builder", "imcms-components-builder"], function (BEM, components) {
+    return {
+        buildHead: function (title, onCloseClick) {
+            var headBEM = new BEM({
+                block: "imcms-head",
+                elements: {
+                    "title": "imcms-title",
+                    "button": ""
+                }
+            });
+
+            var $title = headBEM.buildElement("title", "<div>", {text: title});
+            var $closeBtn = components.buttons.closeButton({click: onCloseClick});
+
+            return headBEM.buildBlock("<div>", [
+                {"title": $title},
+                {"button": $closeBtn}
+            ]);
+        }
+    };
 });
