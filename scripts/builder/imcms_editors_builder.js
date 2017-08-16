a0686c3ea71f2ec9220e58f63bc063bf99e23259
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-16 10:44:47 +0300

    Added content manager builder to editors builder

--- scripts/builder/imcms_editors_builder.js
+++ scripts/builder/imcms_editors_builder.js
@@ -3,12 +3,16 @@
  * 07.08.17.
  */
 Imcms.define("imcms-editors-builder",
-    ["imcms-page-info-builder", "imcms-menu-editor-builder", "imcms-document-editor-builder"],
-    function (pageInfoBuilder, menuEditorBuilder, documentEditorBuilder) {
+    [
+        "imcms-page-info-builder", "imcms-menu-editor-builder", "imcms-document-editor-builder",
+        "imcms-content-manager-builder"
+    ],
+    function (pageInfoBuilder, menuEditorBuilder, documentEditorBuilder, contentManagerBuilder) {
         return {
             buildMenuEditor: menuEditorBuilder.build.bind(menuEditorBuilder),
             buildPageInfo: pageInfoBuilder.build.bind(pageInfoBuilder),
-            buildDocumentEditor: documentEditorBuilder.build.bind(documentEditorBuilder)
+            buildDocumentEditor: documentEditorBuilder.build.bind(documentEditorBuilder),
+            buildContentManager: contentManagerBuilder.build.bind(contentManagerBuilder)
         };
     }
 );
