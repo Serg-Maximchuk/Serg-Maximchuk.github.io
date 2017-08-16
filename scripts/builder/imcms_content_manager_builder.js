d6bb7c6a2f5816107284f0b206b67903a88fcff4
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-16 12:28:08 +0300

    Implemented content manager skeleton.

--- scripts/builder/imcms_content_manager_builder.js
+++ scripts/builder/imcms_content_manager_builder.js
@@ -2,10 +2,58 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 16.08.17.
  */
-Imcms.define("imcms-content-manager-builder", [], function () {
-    return {
-        build: function () {
+Imcms.define("imcms-content-manager-builder", ["imcms-bem-builder", "imcms-window-components-builder", "jquery"],
+    function (BEM, windowComponents, $) {
+        var $contentManager;
 
+        function buildContentManager() {
+            function buildHead() {
+                return $("<div>");
+            }
+
+            function buildFolders() {
+                return $("<div>");
+            }
+
+            function buildImages() {
+                return $("<div>");
+            }
+
+            function buildFooter() {
+                return $("<div>");
+            }
+
+            var contentManagerBEM = new BEM({
+                block: "imcms-content-manager",
+                elements: {
+                    "head": "imcms-head",
+                    "left-side": "imcms-left-side",
+                    "right-side": "imcms-right-side",
+                    "footer": "imcms-footer"
+                }
+            });
+
+            var $head = buildHead();
+            var $folders = buildFolders();
+            var $images = buildImages();
+            var $footer = buildFooter();
+
+            return contentManagerBEM.buildBlock("<div>", [
+                {"head": $head},
+                {"left-side": $folders},
+                {"right-side": $images},
+                {"footer": $footer}
+            ]);
         }
-    };
-});
+
+        return {
+            build: function () {
+                if (!$contentManager) {
+                    $contentManager = buildContentManager().appendTo("body");
+                }
+
+                $contentManager.css("display", "block");
+            }
+        };
+    }
+);
