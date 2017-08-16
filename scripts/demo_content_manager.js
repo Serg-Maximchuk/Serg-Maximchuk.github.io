a5bc90e4607d033acd7e143ffbf26e44e06c1967
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-16 10:45:46 +0300

    Used old and new JS in content manager demo script.

--- scripts/demo_content_manager.js
+++ scripts/demo_content_manager.js
@@ -2,6 +2,17 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 16.08.17.
  */
-Imcms.require(["imcms-content-manager-builder"], function (contentManagerBuilder) {
+Imcms.require(
+    [
+        "imcms-editors-builder", "imcms-folders", "imcms-choose-image", "imcms-content-manager"
+    ],
+    function (editors, imcmsFolders, imcmsChooseImg, imcmsContentManager) {
+        // old JS
+        imcmsFolders.init();
+        imcmsChooseImg.init();
+        imcmsContentManager.init();
 
-});
+        // new JS
+        editors.buildContentManager();
+    }
+);
