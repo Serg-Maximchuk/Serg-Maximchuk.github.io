d9d3bb31f22e6ae2674feb4bc10120985fa5acfc
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-17 12:39:23 +0300

    Rewritten root folder controls build.

--- scripts/loaders/imcms_image_content_loader.js
+++ scripts/loaders/imcms_image_content_loader.js
@@ -2,17 +2,61 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 16.08.17.
  */
-Imcms.define("imcms-image-content-loader",
-    ["imcms-folders-rest-api", "imcms-folders-tree-parser", "imcms-folders"],
-    function (foldersREST, foldersTreeParser, imcmsFolders) {
+Imcms.define("imcms-image-content-loader", //todo: rename to images content builder and move
+    ["imcms-files-rest-api", "imcms-bem-builder"],
+    function (fileREST, BEM) {
         var $foldersContainer, $imagesContainer;
 
-        function loadFoldersContent(foldersArr) {
-            var foldersTree = foldersTreeParser.parseFoldersTree(foldersArr);
-            var $folders = imcmsFolders.buildFolders(foldersTree);
-            $foldersContainer.append($folders);
+        var viewModel = {
+            root: {},
+            $folders: [],
+            $images: []
+        };
 
-            // todo: implement images load!!!!1!
+        function createNewFolder() {
+            // todo: implement
+        }
+
+        function buildRootControls() {
+            var rootFolderControlsBEM = new BEM({
+                block: "imcms-main-folders-controls",
+                elements: {
+                    "control": "imcms-control"
+                }
+            });
+
+            var $createFolderControl = rootFolderControlsBEM.buildElement("control", "<div>", {
+                click: createNewFolder
+            }, ["create"]);
+
+            return rootFolderControlsBEM.buildBlock("<div>", [{"control": $createFolderControl}]);
+        }
+
+        function buildRootFolder(folder) {
+            var rootFolderBEM = new BEM({
+                block: "imcms-left-side",
+                elements: {
+                    "controls": "imcms-main-folders-controls",
+                    "folders": "imcms-folders"
+                }
+            });
+
+            var $rootControls = buildRootControls();
+
+            return rootFolderBEM.makeBlockElement("controls", $rootControls);
+        }
+
+        function buildImages(folder) {
+            return [];
+        }
+
+        function loadImageFoldersContent(folders) {
+            viewModel.root = folders[0];
+            viewModel.$folders.unshift(buildRootFolder(viewModel.root));
+            viewModel.$images.unshift(buildImages(viewModel.root));
+
+            $foldersContainer.append(viewModel.$folders);
+            $imagesContainer.append(viewModel.$images);
         }
 
         return {
@@ -20,7 +64,7 @@ Imcms.define("imcms-image-content-loader",
                 $foldersContainer = options.foldersContainer;
                 $imagesContainer = options.imagesContainer;
 
-                foldersREST.read("/", loadFoldersContent);
+                fileREST.read("/images", loadImageFoldersContent);
             }
         };
     }
