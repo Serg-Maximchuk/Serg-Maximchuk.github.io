6ff2a91b7f35aa4a2b734484d24716a0152bd43c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-19 15:39:32 +0300

    Implemented controls builder module logic.

--- scripts/builder/imcms_controls_builder.js
+++ scripts/builder/imcms_controls_builder.js
@@ -2,6 +2,28 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 19.08.17.
  */
-Imcms.define("imcms-controls-builder", [], function () {
-    return {};
+Imcms.define("imcms-controls-builder", ["imcms-bem-builder"], function (BEM) {
+    var controlsBEM = new BEM({
+        // no block here
+        elements: {"control": "imcms-control"}
+    });
+
+    function buildControl(modifier, onClick) {
+        return controlsBEM.buildElement("control", "<div>", {click: onClick}, [modifier]);
+    }
+
+    return {
+        move: function (onClick) {
+            return buildControl("move", onClick);
+        },
+        remove: function (onClick) {
+            return buildControl("remove", onClick);
+        },
+        rename: function (onClick) {
+            return buildControl("rename", onClick);
+        },
+        create: function (onClick) {
+            return buildControl("create", onClick);
+        }
+    };
 });
