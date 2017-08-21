ff547e5a44821973432e526670b4367b95e28ed0
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-21 17:16:36 +0300

    Used another init js file for image editor example.

--- scripts/imcms_start.js
+++ scripts/imcms_start.js
@@ -2,6 +2,7 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 07.08.17.
  */
-Imcms.require(["imcms-admin-panel-builder"], function (panelBuilder) {
-    panelBuilder.buildPanel();
+Imcms.require(["imcms-admin-panel-builder", "imcms-initialize"], function (panelBuilder, initialize) {
+    initialize.init();
+    // panelBuilder.buildPanel();
 });
