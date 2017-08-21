1e829867d743416dd786da58c079a443766c3ec2
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-21 17:42:14 +0300

    Declared new dependency for image editor builder

--- scripts/builder/imcms_image_editor_builder.js
+++ scripts/builder/imcms_image_editor_builder.js
@@ -2,30 +2,33 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 21.08.17
  */
-Imcms.define("imcms-image-editor-builder", ["imcms-bem-builder", "jquery"], function (BEM, $) {
-    var $editor;
+Imcms.define("imcms-image-editor-builder",
+    ["imcms-bem-builder", "imcms-window-components-builder", "jquery"],
+    function (BEM, windowComponents, $) {
+        var $editor;
 
-    function buildEditor() {
-        var imageEditorBEM = new BEM({
-            block: "imcms-image_editor",
-            elements: {
-                "head": "imcms-head",
-                "image-characteristics": "",
-                "left-side": "",
-                "right-side": ""
-            }
-        });
+        function buildEditor() {
+            var imageEditorBEM = new BEM({
+                block: "imcms-image_editor",
+                elements: {
+                    "head": "imcms-head",
+                    "image-characteristics": "",
+                    "left-side": "",
+                    "right-side": ""
+                }
+            });
 
-        return $("<div>");
-    }
-
-    return {
-        build: function () {
-            if (!$editor) {
-                $editor = buildEditor().prependTo("body");
-            }
-
-            $editor.css("display", "block");
+            return $("<div>");
         }
-    };
-});
+
+        return {
+            build: function () {
+                if (!$editor) {
+                    $editor = buildEditor().prependTo("body");
+                }
+
+                $editor.css("display", "block");
+            }
+        };
+    }
+);
