610d7ab94c0bf3cec5914816176176a8fdae1b8b
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-21 17:38:30 +0300

    Implemented simple build logic without editor body (yet)

--- scripts/builder/imcms_image_editor_builder.js
+++ scripts/builder/imcms_image_editor_builder.js
@@ -2,10 +2,20 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 21.08.17
  */
-Imcms.define("imcms-image-editor-builder", [], function () {
+Imcms.define("imcms-image-editor-builder", ["jquery"], function ($) {
+    var $editor;
+
+    function buildEditor() {
+        return $("<div>");
+    }
+
     return {
         build: function () {
+            if (!$editor) {
+                $editor = buildEditor().prependTo("body");
+            }
 
+            $editor.css("display", "block");
         }
     };
 });
