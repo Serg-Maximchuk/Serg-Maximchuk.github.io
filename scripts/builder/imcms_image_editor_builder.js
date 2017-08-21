b3fdf64c22889cf6f1bf214393f418f8fd6d6dc1
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-21 17:40:59 +0300

    Declared main image editor BEM structure.

--- scripts/builder/imcms_image_editor_builder.js
+++ scripts/builder/imcms_image_editor_builder.js
@@ -2,10 +2,20 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 21.08.17
  */
-Imcms.define("imcms-image-editor-builder", ["jquery"], function ($) {
+Imcms.define("imcms-image-editor-builder", ["imcms-bem-builder", "jquery"], function (BEM, $) {
     var $editor;
 
     function buildEditor() {
+        var imageEditorBEM = new BEM({
+            block: "imcms-image_editor",
+            elements: {
+                "head": "imcms-head",
+                "image-characteristics": "",
+                "left-side": "",
+                "right-side": ""
+            }
+        });
+
         return $("<div>");
     }
 
