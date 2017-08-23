9c29fd0c902a063eb8dfb7204468368b4f068f53
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-23 18:44:22 +0300

    Fixed start script

--- scripts/imcms_start.js
+++ scripts/imcms_start.js
@@ -2,10 +2,6 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 07.08.17.
  */
-Imcms.require(["imcms-admin-panel-builder", "imcms-editors-builder", "imcms-initialize"],
-    function (panelBuilder, editors, initialize) {
-        editors.buildImageEditor();
-        initialize.init();
-    // panelBuilder.buildPanel();
-    }
-);
+Imcms.require(["imcms-admin-panel-builder"], function (panelBuilder) {
+    panelBuilder.buildPanel();
+});
