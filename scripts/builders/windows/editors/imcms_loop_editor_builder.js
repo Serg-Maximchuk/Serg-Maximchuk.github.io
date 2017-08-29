98970e580df7ec3cf80e5f17b053a0b59bffd922
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-29 15:59:23 +0300

    Added simple loop editor skeleton build logic

--- scripts/builders/windows/editors/imcms_loop_editor_builder.js
+++ scripts/builders/windows/editors/imcms_loop_editor_builder.js
@@ -2,10 +2,29 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 29.08.17
  */
-Imcms.define("imcms-loop-editor-builder", [], function () {
+Imcms.define("imcms-loop-editor-builder", ["imcms-bem-builder"], function (BEM) {
+    var $editor;
+
+    function buildEditor() {
+        var editorBEM = new BEM({
+            block: "imcms-loop-editor",
+            elements: {
+                "head": "imcms-head",
+                "body": "imcms-loop-editor-body",
+                "footer": "imcms-footer"
+            }
+        });
+
+        return editorBEM.buildBlock("<div>", [], {"class": "imcms-editor-window"});
+    }
+
     return {
         build: function () {
+            if (!$editor) {
+                $editor = buildEditor().appendTo("body");
+            }
 
+            $editor.css("display", "block");
         }
     }
 });
