682920c1bba21b305f3aa5e76e6e95022a5e9f8b
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-29 16:48:33 +0300

    Build Loop Editor's body while data loading

--- scripts/builders/windows/editors/imcms_loop_editor_builder.js
+++ scripts/builders/windows/editors/imcms_loop_editor_builder.js
@@ -5,13 +5,54 @@
 Imcms.define("imcms-loop-editor-builder",
     ["imcms-bem-builder", "imcms-components-builder", "imcms-window-components-builder"],
     function (BEM, components, windowComponents) {
-        var $editor, $head;
+        var $editor, $head, $body;
 
         function buildEditor() {
             function closeEditor() {
                 $editor.css("display", "none");
             }
 
+            function onCreateNewClicked() {
+                // todo: implement!!!1!
+            }
+
+            function onSaveAndCloseClicked() {
+                closeEditor();
+                // todo: implement saving, for now it's just closing!!1
+            }
+
+            var editorBEM = new BEM({
+                block: "imcms-loop-editor",
+                elements: {
+                    "head": "imcms-head",
+                    "body": "imcms-loop-editor-body",
+                    "footer": "imcms-footer"
+                }
+            });
+
+            $head = windowComponents.buildHead("Loop Editor", closeEditor);
+            $body = editorBEM.buildElement("body", "<div>");
+            var $footer = windowComponents.buildFooter([
+                components.buttons.positiveButton({
+                    text: "Create new",
+                    click: onCreateNewClicked
+                }),
+                components.buttons.saveButton({
+                    text: "Save and close",
+                    click: onSaveAndCloseClicked
+                })
+            ]);
+
+            return editorBEM.buildBlock("<div>", [
+                    {"head": $head},
+                    {"body": $body},
+                    {"footer": $footer}
+                ],
+                {"class": "imcms-editor-window"}
+            );
+        }
+
+        function loadData(opts) {
             function buildTitles() {
                 var titlesBEM = new BEM({
                     block: "imcms-loop-list-titles",
@@ -46,67 +87,24 @@ Imcms.define("imcms-loop-editor-builder",
                 });
 
                 var $titles = buildTitles();
+                // todo: build items here
 
                 return listBEM.buildBlock("<div>", [
                     {"titles": $titles}
                 ]);
             }
 
-            function onCreateNewClicked() {
-                // todo: implement!!!1!
-            }
+            $head.find(".imcms-title").append(": " + opts.docId + "-" + opts.loopId);
 
-            function onSaveAndCloseClicked() {
-                closeEditor();
-                // todo: implement saving, for now it's just closing!!1
-            }
-
-            function buildBody() {
-                var bodyBEM = new BEM({
-                    block: "imcms-loop-editor-body",
-                    elements: {
-                        "list": "imcms-loop-list"
-                    }
-                });
-
-                var $list = buildLoopList();
-
-                return bodyBEM.buildBlock("<div>", [{"list": $list}]);
-            }
-
-            var editorBEM = new BEM({
-                block: "imcms-loop-editor",
+            var bodyBEM = new BEM({
+                block: "imcms-loop-editor-body",
                 elements: {
-                    "head": "imcms-head",
-                    "body": "imcms-loop-editor-body",
-                    "footer": "imcms-footer"
+                    "list": "imcms-loop-list"
                 }
             });
 
-            $head = windowComponents.buildHead("Loop Editor", closeEditor);
-            var $body = buildBody();
-            var $footer = windowComponents.buildFooter([
-                components.buttons.positiveButton({
-                    text: "Create new",
-                    click: onCreateNewClicked
-                }),
-                components.buttons.saveButton({
-                    text: "Save and close",
-                    click: onSaveAndCloseClicked
-                })
-            ]);
-
-            return editorBEM.buildBlock("<div>", [
-                    {"head": $head},
-                    {"body": $body},
-                    {"footer": $footer}
-                ],
-                {"class": "imcms-editor-window"}
-            );
-        }
-
-        function loadData(opts) {
-            $head.find(".imcms-title").append(": " + opts.docId + "-" + opts.loopId);
+            var $list = bodyBEM.makeBlockElement("list", buildLoopList());
+            $body.append($list);
         }
 
         return {
