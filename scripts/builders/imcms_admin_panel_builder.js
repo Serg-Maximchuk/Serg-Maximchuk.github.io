c3805bfc0bb462bfc17d6406fdb5fdfbec0370e9
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-08-30 11:48:13 +0300

    Admin panel builder refactoring

--- scripts/builders/imcms_admin_panel_builder.js
+++ scripts/builders/imcms_admin_panel_builder.js
@@ -7,8 +7,13 @@ Imcms.define("imcms-admin-panel-builder",
     function (BEM, componentsBuilder, editors, $) {
         var $panel;
 
-        function logNotImplementedFeature(feature) {
-            console.log("%c Not implemented feature: " + feature, "color: red;");
+        var panelSensitivePixels = 15;
+
+        function getNotImplementedButtonClick(feature) {
+            return function (e) {
+                e.preventDefault();
+                console.log("%c Not implemented feature: " + feature, "color: red;");
+            }
         }
 
         function showPageInfo() {
@@ -17,6 +22,77 @@ Imcms.define("imcms-admin-panel-builder",
 
         function initDocumentEditor() {
             editors.buildDocumentEditor();
+        }
+
+        function buildPanelButtons() {
+            var panelButtonsBEM = new BEM({
+                block: "imcms-menu",
+                elements: {
+                    "items": "",
+                    "item": "imcms-menu__item"
+                }
+            });
+
+            function buildPanelButton(buttonData) {
+                return panelButtonsBEM.buildBlockElement("item", "<li>", {
+                    html: buttonData.content,
+                    click: buttonData.onClick
+                }, buttonData.modifiers);
+            }
+
+            var buttonsData = [
+                {
+                    content: "public",
+                    onClick: getNotImplementedButtonClick("public view click"),
+                    modifiers: ["public"]
+                }, {
+                    content: "edit",
+                    onClick: getNotImplementedButtonClick("edit click"),
+                    modifiers: ["edit"]
+                }, {
+                    content: "preview",
+                    onClick: getNotImplementedButtonClick("preview click"),
+                    modifiers: ["preview"]
+                }, {
+                    content: "publish offline",
+                    onClick: getNotImplementedButtonClick("publish offline version click"),
+                    modifiers: ["publish-of"]
+                }, {
+                    content: "page info",
+                    onClick: showPageInfo,
+                    modifiers: ["page-info"]
+                }, {
+                    content: "document",
+                    onClick: initDocumentEditor,
+                    modifiers: ["document"]
+                }, {
+                    content: "admin",
+                    onClick: getNotImplementedButtonClick("admin click"),
+                    modifiers: ["admin"]
+                }, {
+                    content: componentsBuilder.buttons.positiveButton({
+                        text: "log out",
+                        click: getNotImplementedButtonClick("logout click")
+                    }),
+                    modifiers: ["logout"]
+                }
+            ];
+
+            var buttons = buttonsData.map(buildPanelButton);
+            var $buttonsWrapper = $("<ul>").append(buttons);
+
+            return panelButtonsBEM.buildBlock("<div>", [{"items": $buttonsWrapper}]);
+        }
+
+        function buildFlags() {
+            return componentsBuilder.flags.flagsContainer("<div>", [
+                componentsBuilder.flags.eng("<div>", true, {
+                    click: getNotImplementedButtonClick("flags click")
+                }),
+                componentsBuilder.flags.swe("<div>", false, {
+                    click: getNotImplementedButtonClick("flags click")
+                })
+            ]);
         }
 
         function createAdminPanel() {
@@ -29,109 +105,13 @@ Imcms.define("imcms-admin-panel-builder",
                 }
             });
 
-            var panelButtonsBEM = new BEM({
-                block: "imcms-menu",
-                elements: {
-                    "items": "",
-                    "item": "imcms-menu__item"
-                }
-            });
-
-            var $logo = adminPanelBEM.buildBlockElement("logo", "<a>", {
-                href: "#" // maybe it should be the link to the start page?
-            });
+            var $logo = adminPanelBEM.buildBlockElement("logo", "<a>", {href: ""}); // fixme: link to start doc?
             var $logoItem = $("<div>").append($logo);
-            var $title = adminPanelBEM.buildBlockElement("title", "<div>", {
-                text: Imcms.version
-            });
+            var $title = adminPanelBEM.buildBlockElement("title", "<div>", {text: Imcms.version});
             var $titleItem = $("<div>").append($title);
-            var $flagsItem = componentsBuilder.flags.flagsContainer("<div>", [
-                componentsBuilder.flags.eng("<div>", true, {
-                    click: function (e) {
-                        e.preventDefault();
-                        logNotImplementedFeature("flags click");
-                    }
-                }),
-                componentsBuilder.flags.swe("<div>", false, {
-                    click: function (e) {
-                        e.preventDefault();
-                        logNotImplementedFeature("flags click");
-                    }
-                })
-            ]);
-            var $buttonsWrapper = $("<ul>");
-            var $publicViewButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "public",
-                click: function (e) {
-                    e.preventDefault();
-                    logNotImplementedFeature("public view click");
-                }
-            }, ["public"]);
 
-            var $editPageButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "edit",
-                click: function (e) {
-                    e.preventDefault();
-                    logNotImplementedFeature("edit click");
-                }
-            }, ["edit"]);
-
-            var $previewButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "preview",
-                click: function (e) {
-                    e.preventDefault();
-                    logNotImplementedFeature("preview click");
-                }
-            }, ["preview"]);
-
-            var $publishOfflineVersionButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "publish offline",
-                click: function (e) {
-                    e.preventDefault();
-                    logNotImplementedFeature("publish offline version click");
-                }
-            }, ["publish-of"]);
-
-            var $pageInfoButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "page info",
-                click: showPageInfo
-            }, ["page-info"]);
-
-            var $openDocumentEditorButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "document",
-                click: initDocumentEditor
-            }, ["document"]);
-
-            var $adminPageButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                text: "admin",
-                click: function (e) {
-                    e.preventDefault();
-                    logNotImplementedFeature("admin click");
-                }
-            }, ["admin"]);
-
-            var $logoutButton = panelButtonsBEM.buildBlockElement("item", "<li>", {
-                html: componentsBuilder.buttons.positiveButton({
-                    text: "log out",
-                    click: function (e) {
-                        e.preventDefault();
-                        logNotImplementedFeature("logout click");
-                    }
-                })
-            }, ["logout"]);
-
-            $buttonsWrapper.append($publicViewButton)
-                .append($editPageButton)
-                .append($previewButton)
-                .append($publishOfflineVersionButton)
-                .append($pageInfoButton)
-                .append($openDocumentEditorButton)
-                .append($adminPageButton)
-                .append($logoutButton);
-
-            var $buttonsContainer = panelButtonsBEM.buildBlock("<div>", [
-                {"items": $buttonsWrapper}
-            ]);
+            var $flagsItem = buildFlags();
+            var $buttonsContainer = buildPanelButtons();
 
             return adminPanelBEM.buildBlock("<div>", [
                 {"item": $logoItem},
@@ -141,11 +121,9 @@ Imcms.define("imcms-admin-panel-builder",
             ]);
         }
 
-        var panelSensitivePixels = 15;
-
         function setShowPanelRule() {
             $(document).mousemove(function (event) {
-                if (event.pageY >= 0 && event.pageY <= panelSensitivePixels) {
+                if ((event.pageY >= 0) && (event.pageY <= panelSensitivePixels)) {
                     showPanel();
                 }
             });
