b0f068e58a5395ceb144a2d5e74a6620e63161f0
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-01 19:17:55 +0300

    Replaced scratch tinymce init by text editor module api usage

--- scripts/demo_edit.js
+++ scripts/demo_edit.js
@@ -5,9 +5,9 @@
 Imcms.require(
     [
         "imcms-date-picker", "imcms-time-picker", "imcms-tests", "imcms-components-builder", "imcms-bem-builder",
-        "imcms-editors-builder", "imcms-admin-panel-builder", "jquery", "tinyMCE"
+        "imcms-editors-builder", "imcms-admin-panel-builder", "imcms-text-editor", "jquery"
     ],
-    function (DatePicker, TimePicker, tests, componentsBuilder, BEM, editors, panelBuilder, $, tinyMCE) {
+    function (DatePicker, TimePicker, tests, componentsBuilder, BEM, editors, panelBuilder, textEditor, $) {
         Imcms.tests = tests;
         console.info("%c Tests loaded.", "color: green");
 
@@ -46,30 +46,8 @@ Imcms.require(
 
         $("#links-as-buttons").append($demoEditorsBtnsContainer);
 
-        // stupid way to get contextPath! todo: receive from server
-        var relativePath = window.location.pathname;
-        var contextPath = ((relativePath.lastIndexOf("/") === 0) ? "" : "/" + relativePath.split("/")[1]);
-
-        tinyMCE.init({
-            skin_url: (contextPath + '/libs/tinymce/skins/white'),
-            selector: '.imcms-text-edit-area--inline',
-            inline: true,
-            skin: 'white',
-            plugins: ['autolink link image lists hr code fullscreen save table contextmenu'],
-            toolbar: 'code | bold italic underline | bullist numlist | hr | alignleft aligncenter alignright alignjustify | link image | fullscreen | save',
-            menubar: false,
-            statusbar: false
-        });
-        tinyMCE.init({
-            skin_url: (contextPath + '/libs/tinymce/skins/white'),
-            selector: '.imcms-text-edit-area--form',
-            skin: 'white',
-            plugins: ['autolink link image lists hr code fullscreen save table contextmenu autoresize'],
-            toolbar: 'code | bold italic underline | bullist numlist | hr | alignleft aligncenter alignright alignjustify | link image | fullscreen | save',
-            autoresize_bottom_margin: 20,
-            menubar: false,
-            statusbar: false
-        });
+        textEditor.initTextEditorClassic('.imcms-text-edit-area--form');
+        textEditor.initTextEditorInline('.imcms-text-edit-area--inline');
 
         console.timeEnd("imCMS JS loaded");
     }
