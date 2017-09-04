966d8ebef539831a5963b18dbc4b457291c3a562
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-04 14:56:44 +0300

    Implemented image editor init module logic

--- scripts/editor_initializer/imcms_image_editor_initializer.js
+++ scripts/editor_initializer/imcms_image_editor_initializer.js
@@ -2,6 +2,15 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 04.09.17
  */
-Imcms.define("imcms-image-editor-initializer", [], function () {
-    return {}
+Imcms.define("imcms-image-editor-initializer", ["imcms-editors-builder", "jquery"], function (editorsBuilder, $) {
+    function openEditor() {
+        var imageId = $(this).parents(".imcms-editor-area--image").find(".imcms-editor-content").data("imageId");
+        editorsBuilder.buildImageEditor(imageId);
+    }
+
+    return {
+        initEditor: function () {
+            $(".imcms-editor-area--image").find(".imcms-control--image").click(openEditor);
+        }
+    }
 });
