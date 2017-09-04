28955a9061445160edd949e6f5122d7dc8ea3609
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-04 14:57:05 +0300

    Included image editor init module to editors initializer

--- scripts/editor_initializer/imcms_editors_initializer.js
+++ scripts/editor_initializer/imcms_editors_initializer.js
@@ -2,18 +2,19 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 04.09.17
  */
-Imcms.define("imcms-editors-initializer", ["imcms-text-editor-initializer"], function (textEditorInit) {
-    var editors = [
-        textEditorInit
-    ];
+Imcms.define("imcms-editors-initializer",
+    ["imcms-text-editor-initializer", "imcms-image-editor-initializer"],
+    function (textEditorInit, imageEditorInit) {
+        var editors = Array.prototype.slice.call(arguments);
 
-    function initEditor(editor) {
-        editor.initEditor();
-    }
-
-    return {
-        initEditors: function () {
-            editors.forEach(initEditor);
+        function initEditor(editor) {
+            editor.initEditor();
         }
-    };
-});
+
+        return {
+            initEditors: function () {
+                editors.forEach(initEditor);
+            }
+        };
+    }
+);
