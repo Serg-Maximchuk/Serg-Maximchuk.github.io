deffca68649b062e5026382ea122ed99a7b683a5
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-04 19:00:06 +0300

    Simplified edit demo page js

--- scripts/demo_edit.js
+++ scripts/demo_edit.js
@@ -3,49 +3,12 @@
  * 01.09.17
  */
 Imcms.require(
-    [
-        "imcms-date-picker", "imcms-time-picker", "imcms-tests", "imcms-components-builder", "imcms-bem-builder",
-        "imcms-editors-builder", "imcms-admin-panel-builder", "imcms-editors-initializer", "jquery"
-    ],
-    function (DatePicker, TimePicker, tests, componentsBuilder, BEM, editors, panelBuilder, editorsInit, $) {
+    ["imcms-tests", "imcms-admin-panel-builder", "imcms-editors-initializer"],
+    function (tests, panelBuilder, editorsInit) {
         Imcms.tests = tests;
         console.info("%c Tests loaded.", "color: green");
 
         panelBuilder.buildPanel({active: 'edit'});
-
-        var buttons = componentsBuilder.buttons;
-
-        var $chooseImgLink = buttons.positiveButton({
-                text: "choose image",
-                click: editors.buildContentManager
-            }),
-            $imageEditorLink = buttons.positiveButton({
-                text: "image editor",
-                click: editors.buildImageEditor
-            }),
-            $menuEditorBtn = buttons.positiveButton({
-                text: "menu editor",
-                click: editors.buildMenuEditor
-            }),
-            $loopEditorBtn = buttons.positiveButton({
-                text: "loop editor",
-                click: function () {
-                    editors.buildLoopEditor({
-                        docId: 1001,
-                        loopId: 1
-                    });
-                }
-            }),
-            $demoEditorsBtnsContainer = buttons.buttonsContainer("<div>", [
-                $chooseImgLink,
-                $imageEditorLink,
-                $menuEditorBtn,
-                $loopEditorBtn
-            ])
-        ;
-
-        $("#links-as-buttons").append($demoEditorsBtnsContainer);
-
         editorsInit.initEditors();
 
         console.timeEnd("imCMS JS loaded");
