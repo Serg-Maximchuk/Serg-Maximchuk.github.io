af99bec5c930ea4d9e921d1597f399d0a4e6ec3c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-05 18:43:06 +0300

    Changed mock response and page images data to improve demo

--- scripts/rest/imcms_image_rest_api.js
+++ scripts/rest/imcms_image_rest_api.js
@@ -10,24 +10,28 @@ Imcms.define("imcms-image-rest-api", ["imcms-rest-api"], function (rest) {
         var mockData = {
             "1001": [
                 {
-                    name: "img1",
-                    path: "img/choose_img/img1.png",
-                    format: "PNG",
-                    width: 146,
-                    height: 114
-                }, {
-                    name: "img2",
-                    path: "img/choose_img/img2.png",
-                    format: "PNG",
-                    width: 147,
-                    height: 146
-                }, {
+                    id: 0,
                     name: "img3",
                     path: "img/choose_img/img3.png",
                     format: "PNG",
                     width: 32,
                     height: 29
                 }, {
+                    id: 1,
+                    name: "img1",
+                    path: "img/choose_img/img1.png",
+                    format: "PNG",
+                    width: 146,
+                    height: 114
+                }, {
+                    id: 2,
+                    name: "img2",
+                    path: "img/choose_img/img2.png",
+                    format: "PNG",
+                    width: 147,
+                    height: 146
+                }, {
+                    id: 3,
                     name: "img4",
                     path: "img/choose_img/img4.png",
                     format: "PNG",
@@ -39,7 +43,7 @@ Imcms.define("imcms-image-rest-api", ["imcms-rest-api"], function (rest) {
 
         return {
             done: function (onDone) {
-                onDone(mockData[data.docId][+data.imageId - 1]);
+                onDone(mockData[data.docId][+data.imageId]);
             }
         }
     };
