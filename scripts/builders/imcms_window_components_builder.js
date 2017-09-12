88135b11fc5364f1c05d2fc6ed8d8255ddecdefb
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-12 11:13:18 +0300

    Refactored BEM builder and it's usage

--- scripts/builders/imcms_window_components_builder.js
+++ scripts/builders/imcms_window_components_builder.js
@@ -5,38 +5,27 @@
 Imcms.define("imcms-window-components-builder", ["imcms-bem-builder", "imcms-components-builder"], function (BEM, components) {
     return {
         buildHead: function (title, onCloseClick) {
-            var headBEM = new BEM({
+            return new BEM({
                 block: "imcms-head",
                 elements: {
-                    "title": "imcms-title",
-                    "button": ""
+                    "title": components.texts.titleText("<div>", title),
+                    "button": components.buttons.closeButton({click: onCloseClick})
                 }
-            });
-
-            var $title = headBEM.buildElement("title", "<div>", {text: title});
-            var $closeBtn = components.buttons.closeButton({click: onCloseClick});
-
-            return headBEM.buildBlock("<div>", [
-                {"title": $title},
-                {"button": $closeBtn}
-            ]);
+            }).buildBlockStructure("<div>");
         },
         buildFooter: function (buttons) {
-            var footerBEM = new BEM({
-                block: "imcms-footer",
-                elements: {
-                    "buttons": "imcms-buttons"
-                }
-            });
-
-            var elements = [];
+            var elements = {};
 
             if (buttons) {
-                var $buttons = components.buttons.buttonsContainer("<div>", buttons);
-                elements = [{"buttons": $buttons}];
+                elements.buttons = components.buttons.buttonsContainer("<div>", buttons);
             }
 
-            return footerBEM.buildBlock("<div>", elements);
+            return new BEM(
+                {
+                    block: "imcms-footer",
+                    elements: elements
+                }
+            ).buildBlockStructure("<div>");
         }
     };
 });
