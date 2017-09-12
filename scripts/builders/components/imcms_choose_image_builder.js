3e71576f31a0a5ea45847417a95e06355d085c12
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-12 11:18:26 +0300

    Refactored BEM builder and it's usage

--- scripts/builders/components/imcms_choose_image_builder.js
+++ scripts/builders/components/imcms_choose_image_builder.js
@@ -5,31 +5,23 @@
 Imcms.define("imcms-choose-image-builder",
     ["imcms-bem-builder", "imcms-texts-builder", "imcms-buttons-builder", "imcms-content-manager-builder"],
     function (BEM, texts, buttons, contentManager) {
-        var chooseImageBEM = new BEM({
-            block: "imcms-choose-image",
-            elements: {
-                "text-box": "imcms-text-box"
-            }
-        });
-
         return {
             container: function (tag, attributes) {
-                var $textBox = texts.textField("<div>", {
-                        id: attributes.id,
-                        name: attributes.name,
-                        text: attributes["label-text"],
-                        placeholder: attributes.placeholder
-                    }),
-
-                    $chooseImageButton = buttons.neutral("<button>", {
-                        text: attributes["button-text"],
-                        click: attributes.click || contentManager.build
-                    });
-
-                return chooseImageBEM.buildBlock("<div>", [
-                    {"text-box": $textBox},
-                    {"button": $chooseImageButton}
-                ]);
+                return new BEM({
+                    block: "imcms-choose-image",
+                    elements: {
+                        "text-box": texts.textField("<div>", {
+                            id: attributes.id,
+                            name: attributes.name,
+                            text: attributes["label-text"],
+                            placeholder: attributes.placeholder
+                        }),
+                        "button": buttons.neutral("<button>", {
+                            text: attributes["button-text"],
+                            click: attributes.click || contentManager.build
+                        })
+                    }
+                }).buildBlockStructure("<div>");
             }
         }
     }
