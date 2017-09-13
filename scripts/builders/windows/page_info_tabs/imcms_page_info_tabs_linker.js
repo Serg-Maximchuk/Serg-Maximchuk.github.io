65bd2c837facae55cd56fc2d47277f93ce239c75
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-13 10:51:13 +0300

    Page info builder components refactoring

--- scripts/builders/windows/page_info_tabs/imcms_page_info_tabs_linker.js
+++ scripts/builders/windows/page_info_tabs/imcms_page_info_tabs_linker.js
@@ -1,15 +1,13 @@
-Imcms.define("imcms-page-info-tabs-linker", ["imcms-bem-builder"],
-    function (BEM) {
+Imcms.define("imcms-page-info-tabs-linker", ["imcms-bem-builder"], function (BEM) {
 
-        var formsBEM = new BEM({
-            block: "imcms-form",
-            elements: {"field": "imcms-field"}
-        });
+    var formsBEM = new BEM({
+        block: "imcms-form",
+        elements: {"field": "imcms-field"}
+    });
 
-        return {
-            buildFormBlock: function (elements, index) {
-                return formsBEM.buildBlock("<div>", elements, {"data-window-id": index}, "field");
-            }
-        };
-    }
-);
+    return {
+        buildFormBlock: function (elements, index) {
+            return formsBEM.buildBlock("<div>", elements, {"data-window-id": index}, "field");
+        }
+    };
+});
