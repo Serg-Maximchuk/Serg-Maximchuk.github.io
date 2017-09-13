2dd43a52cff6888fcf65f545420c0185e041096e
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-13 13:15:21 +0300

    Page info tabs refactoring

--- scripts/builders/windows/page_info_tabs/imcms_permissions_tab_builder.js
+++ scripts/builders/windows/page_info_tabs/imcms_permissions_tab_builder.js
@@ -5,9 +5,10 @@ Imcms.define("imcms-permissions-tab-builder",
     ],
     function (BEM, components, linker) {
 
+        var tabData = {};
+    
         return {
             name: "permissions",
-            data: {},
             buildTab: function (index) {
                 function createRestrictedCheckboxesDependingOnIndex(index) {
                     return mapCheckboxesFromAttributesArray([{
@@ -34,43 +35,35 @@ Imcms.define("imcms-permissions-tab-builder",
                     });
                 }
 
-                var fieldItemBEM = new BEM({
-                    block: "imcms-field",
-                    elements: {
-                        "item": "imcms-col-3"
-                    }
-                });
-
                 var restrictedCheckboxes0 = createRestrictedCheckboxesDependingOnIndex(0);
                 var $restrictedRole1Rights = components.checkboxes.checkboxContainer("<div>",
                     restrictedCheckboxes0,
                     {title: "Restricted 1"}
                 );
+                $restrictedRole1Rights.modifiers = ["float-l", "col-3"];
 
                 var restrictedCheckboxes1 = createRestrictedCheckboxesDependingOnIndex(1);
                 var $restrictedRole2Rights = components.checkboxes.checkboxContainer("<div>",
                     restrictedCheckboxes1,
                     {title: "Restricted 2"}
                 );
+                $restrictedRole2Rights.modifiers = ["float-l", "col-3"];
 
-                this.data.restrictedCheckboxes = restrictedCheckboxes0.concat(restrictedCheckboxes1);
+                tabData.restrictedCheckboxes = restrictedCheckboxes0.concat(restrictedCheckboxes1);
 
-                var $permissionsWrapper = fieldItemBEM.buildBlock("<div>", [{
-                    "item": $restrictedRole1Rights,
-                    modifiers: ["float-l", "col-3"]
-                }, {
-                    "item": $restrictedRole2Rights,
-                    modifiers: ["float-l", "col-3"]
-                }]);
+                var $permissionsWrapper = new BEM({
+                    block: "imcms-field",
+                    elements: {
+                        "item": [$restrictedRole1Rights, $restrictedRole2Rights]
+                    }
+                }).buildBlockStructure("<div>");
 
                 return linker.buildFormBlock([$permissionsWrapper], index);
             },
             fillTabDataFromDocument: function (document) {
-                var permissionsTab = this.data,
+                var restrictedCheckboxes = {};
 
-                    restrictedCheckboxes = {};
-
-                permissionsTab.restrictedCheckboxes.forEach(function (permission) {
+                tabData.restrictedCheckboxes.forEach(function (permission) {
                     restrictedCheckboxes[permission.find("input").prop("name")] = permission;
                 });
 
@@ -89,7 +82,7 @@ Imcms.define("imcms-permissions-tab-builder",
                 });
             },
             clearTabData: function () {
-                this.data.restrictedCheckboxes.forEach(function (checkbox) {
+                tabData.restrictedCheckboxes.forEach(function (checkbox) {
                     checkbox.setChecked(false);
                 });
             }
